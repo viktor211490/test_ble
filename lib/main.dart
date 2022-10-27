bb3a16c60c86ef53abdcaf7fc39df8bf254e83be
ViktorAlekseev <alexeevdva@gmail.com>
2022-10-27 20:32:26 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter BLE Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter BLE Demo'),
     );
   }
 }
@@ -33,12 +33,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   List<BlueScanResult> bleNames = [];
+  var set = <String>{};
+
   _stopScan() {
     QuickBlue.stopScan();
     setState(() {
-      bleNames = bleNames.toSet().toList();
+      bleNames =
+          bleNames.where((element) => set.add(element.deviceId)).toList();
     });
   }
 
@@ -46,16 +48,33 @@ class _MyHomePageState extends State<MyHomePage> {
     return Timer(Duration(milliseconds: milliseconds), _stopScan);
   }
 
-  void _incrementCounter() {
+  void _connectBlePeripheral(BlueScanResult data) {
+    print(data.name);
+
+    void _handleConnectionChange(String deviceId, BlueConnectionState state) {
+      print('_handleConnectionChange $deviceId, ${state.value}');
+    }
+
+    void _handleServiceDiscovery(String deviceId, String serviceId) {
+      print('_handleServiceDiscovery $deviceId, $serviceId');
+    }
+
+    QuickBlue.connect(data.deviceId);
+
+    QuickBlue.setConnectionHandler(_handleConnectionChange);
+    QuickBlue.setServiceHandler(_handleServiceDiscovery);
+  }
+
+  void _disconnectBlePher(BlueScanResult data) {
+    QuickBlue.disconnect(data.deviceId);
+  }
+
+  void _findBleDevices() {
     QuickBlue.startScan();
     QuickBlue.scanResultStream.listen((result) {
       bleNames.add(result);
-      print('onScanResult ' + result.name);
     });
-    scheduleTimeout(5 * 1000);
-    setState(() {
-      _counter++;
-    });
+    scheduleTimeout(10 * 1000);
   }
 
   final spinkit = SpinKitFadingCircle(
@@ -70,47 +89,92 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    _incrementCounter(); //call it over here
+    _findBleDevices();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('hello'),
-        ),
-        body: bleNames.isNotEmpty
-            ? ListView.builder(
-                itemCount: bleNames.length,
-                prototypeItem: ListTile(
-                  title: Text(bleNames.first.name),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: bleNames.isNotEmpty
+          ? ListView.builder(
+              itemCount: bleNames.length,
+              prototypeItem: ListTile(
+                title: Text(bleNames.first.name),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('id: ${bleNames.first.deviceId}'),
+                    Text('rssi: ${bleNames.first.rssi}'),
+                    Text(
+                        'manufacturerData/bufer: ${bleNames.first.manufacturerData.buffer.asByteData()}'),
+                  ],
+                ),
+              ),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(bleNames[index].name.isNotEmpty
+                      ? bleNames[index].name
+                      : "Name is EMPTY"),
                   subtitle: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('id: ${bleNames.first.deviceId}'),
-                      Text('rssi: ${bleNames.first.rssi}'),
-                      Text(
-                          'manufacturerData/bufer: ${bleNames.first.manufacturerData.buffer.asByteData()}'),
+                      Text('id: ${bleNames[index].deviceId}'),
+                      Text('rssi: ${bleNames[index].rssi}'),
                     ],
                   ),
-                ),
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(bleNames[index].name.isNotEmpty
-                        ? bleNames[index].name
-                        : "Name is EMPTY"),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('id: ${bleNames[index].deviceId}'),
-                        Text('rssi: ${bleNames[index].rssi}'),
-                        Text(
-                            'manufacturerData/bufer: ${bleNames[index].manufacturerData.buffer.asByteData()}'),
-                      ],
-                    ),
-                  );
-                },
-              )
-            : spinkit);
+                  onTap: () => {
+                    _connectBlePeripheral(bleNames[index]),
+                  },
+                  onLongPress: () => {_disconnectBlePher(bleNames[index])},
+                  trailing: IconButton(
+                      onPressed: () =>
+                          {_showMyDialog(context, bleNames[index])},
+                      icon: const Icon(Icons.more_vert)),
+                );
+              },
+            )
+          : spinkit,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {
+          setState(() {
+            bleNames.removeRange(0, bleNames.length);
+            set = <String>{};
+          }),
+          _findBleDevices()
+        },
+        child: Icon(Icons.refresh),
+      ),
+    );
   }
+}
+
+Future<void> _showMyDialog(BuildContext context, BlueScanResult bleInfo) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(bleInfo.name.isNotEmpty ? bleInfo.name : bleInfo.deviceId),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text('rssi: ${bleInfo.rssi}'),
+              Text('manufacturerData/bufer: ${bleInfo.name}'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Approve'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
